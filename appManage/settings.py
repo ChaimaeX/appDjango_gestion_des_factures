bb2280d23427d227b9a31b89eaeb7a1c62e4086a
Your Name <you@example.com>
2024-04-28 12:15:46 -0700

    first commit

--- appManage/settings.py
+++ appManage/settings.py
@@ -32,11 +32,11 @@ try:
 except Exception as e:
     pass
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#*k)9t8vzu)4j2hr=l@)emxs^ev%6z5)ri$q4g4%nb^n+pr37y'
+SECRET_KEY =os.environ.get('SECRET_KEY','#*k)9t8vzu)4j2hr=l@)emxs^ev%6z5)ri$q4g4%nb^n+pr37y') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG', 'True')=="True"
+DEBUG = os.environ.get('DEBUG', 'True') == "True"
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'samia-solaire.onrender.com']
 
 
