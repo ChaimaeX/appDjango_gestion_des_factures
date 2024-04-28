a4deed03e5656731135a62cf2329f5524e73d16d
Your Name <you@example.com>
2024-04-28 01:18:50 -0700

    first commit

--- stock/models.py
+++ stock/models.py
@@ -16,8 +16,8 @@ class Facture(models.Model):
     
      client = models.CharField(max_length=132)
      facture_date_time = models.DateTimeField(null = True , auto_now_add=True)
-     save_by = models.ForeignKey(User , on_delete=models.PROTECT )
-     total = models.DecimalField(max_digits=10, decimal_places=2)
+     save_by = models.ForeignKey(User, on_delete=models.PROTECT )
+     total = models.DecimalField(max_digits=10, decimal_places=0)
      last_update_date = models.DateTimeField(null = True, blank=True)
      paid = models.BooleanField(default=False)
      # comments= models.TextField(null = True , max_length =1000, blank=True )
